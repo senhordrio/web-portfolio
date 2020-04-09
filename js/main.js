881e60794e4c3ce47d61425bed7e3a1e4bddc192
senhordrio <adriosilvao@gmail.com>
2020-04-08 23:36:15 -0300

    subindo repositório

---
+++ js/main.js
@@ -0,0 +1,70 @@
+//components
+var $home = $("#home");
+var $about = $("#about");
+var $projects = $("#projects");
+var $contactComponent = $(".contact");
+
+//animations
+var $bodyBackground = $(".bodyWhiteBackgroundColor");
+
+
+//buttons
+var $navigateToAbout = $(".navigateToAbout");
+var $navigateToProjects = $(".navigateToProjects");
+var $navigateToHomeFromProjects = $(".navigateToHomeFromProjects");
+var $navigateToHomeFromAbout = $(".navigateToHomeFromAbout");
+var $contactPosition = $(".contact");
+var $contactColors = $(".buttonEffect");
+
+//methods
+
+$navigateToAbout.click(function () {
+  $home.hide();
+  $bodyBackground.removeClass("bodyWhiteBackgroundColor").addClass("bodyBlackBackgroundColor");
+  $about.show();
+  $contactPosition.css({ "transition": "0.2s", "left": "435px" });
+  $contactColors.removeClass("buttonEffect").addClass("buttonEffectOnAbout");
+});
+
+$navigateToHomeFromAbout.click(function(){
+  $about.hide();
+  $contactPosition.css({"left": "200px" });
+  $bodyBackground.removeClass("bodyBlackBackgroundColor").addClass("bodyWhiteBackgroundColor");
+  $contactColors.removeClass("buttonEffectOnAbout").addClass("buttonEffect");
+  $home.addClass("fadeIn");
+  $home.show();
+})
+
+$navigateToProjects.click(function () {
+  $home.hide();
+  $bodyBackground.removeClass("bodyWhiteBackgroundColor").addClass("bodyBlackBackgroundColor");
+  $contactComponent.hide();
+  $projects.show();
+});
+
+$navigateToHomeFromProjects.click(function(){
+  $projects.hide();
+  $bodyBackground.removeClass("bodyBlackBackgroundColor").addClass("bodyWhiteBackgroundColor");
+  $contactColors.removeClass("buttonEffectOnAbout").addClass("buttonEffect");
+  $home.addClass("fadeIn");
+  $home.show();
+  $contactComponent.addClass("fadeIn")
+  $contactComponent.show();
+})
+
+VANTA.NET({
+  el: ".homeAnimation",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 350.0,
+  minWidth: 400.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x0,
+  points: 5.0,
+  maxDistance: 21.0,
+  spacing: 10.0,
+  color: 0xffffff,
+  backgroundColor: 0x0,
+  showDots: true,
+});
